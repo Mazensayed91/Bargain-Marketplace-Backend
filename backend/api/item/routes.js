d4268a85c045c1e4f16a262ba0563da0ad0c70e9
mahmoudyusof <mahmoud.yusof27@gmail.com>
2021-12-29 20:11:36 +0200

    permissions

--- backend/api/item/routes.js
+++ backend/api/item/routes.js
@@ -6,6 +6,7 @@ const {
   delete_item,
   get_all_items,
   get_all_items_by_user,
+  get_all_items_by_user_and_owners_items
 } = require("./controller");
 
 router.get("/:id", get_item);
@@ -16,7 +17,7 @@ router.put("/:id", edit_item);
 
 router.delete("/delete", delete_item);
 
-router.get("/user/:id", get_all_items_by_user);
+router.get("/user/:id", get_all_items_by_user_and_owners_items);
 
 router.get("/", get_all_items);
 
--- backend/api/user/routes.js
+++ backend/api/user/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const auth_middleware = require("../../auth/auth")
+// const auth_middleware = require("../../auth/auth")
 const { get_all_users, get_user_by_id, permit_user } = require("./controller");
-router.use(auth_middleware);
+// router.use(auth_middleware);
 router.get("/", get_all_users);
 router.get("/:id", get_user_by_id);
 router.post("/permit", permit_user)
