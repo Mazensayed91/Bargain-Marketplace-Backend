e5f2fad79ee5a9762cc8d95709928f2fd42ea587
mahmoudyusof <mahmoud.yusof27@gmail.com>
2021-12-29 20:11:36 +0200

    permissions

--- backend/api/user/routes.js
+++ backend/api/user/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const { get_all_users, get_user_by_id } = require("./controller");
-
+// const auth_middleware = require("../../auth/auth")
+const { get_all_users, get_user_by_id, permit_user } = require("./controller");
+// router.use(auth_middleware);
 router.get("/", get_all_users);
 router.get("/:id", get_user_by_id);
+router.post("/permit", permit_user)
 module.exports = router;
