50ed8985eb8e7cd982dc02d72540d655c255182c
atout811 <dmcgfx811@gmail.com>
2021-12-29 22:32:53 +0200

    test

--- backend/api/cart/controller.js
+++ backend/api/cart/controller.js
@@ -1,31 +1,34 @@
-const { CartItem, Item } = require("../../models")
+const { CartItem, Item } = require("../../models");
 
 exports.addToCart = async (req, res) => {
-    try{
-        const cart_item = await CartItem.create({
-            user_id: req.body.user_id,
-            item_id: req.body.item_id,
-        });
+  try {
+    const cart_item = await CartItem.create({
+      user_id: req.body.user_id,
+      item_id: req.body.item_id,
+    });
 
-        return res.json({"item": cart_item});
-    }catch(e){
-        return res.status(500).json({"error": e.message});
-    }
+    return res.json({ item: cart_item });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
 };
 
 exports.removeFromCart = async (req, res) => {
-    try{
-        await CartItem.destroy({where: {id: req.body.id}})
-        return res.json({message: "Item removed from cart"})
-    }catch(e){
-        return res.status(500).json({error: e.message})
-    }
+  try {
+    await CartItem.destroy({ where: { id: req.body.id } });
+    return res.json({ message: true });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
 };
 exports.getUserCart = async (req, res) => {
-    try{
-        const cart_items = await CartItem.findAll({where: {user_id: req.params.user_id}, include: [Item]})
-        return res.json({cart: cart_items})
-    }catch(e) {
-        return res.status(500).json({error: e.message})
-    }
+  try {
+    const cart_items = await CartItem.findAll({
+      where: { user_id: req.params.user_id },
+      include: [Item],
+    });
+    return res.json({ cart: cart_items });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
+  }
 };
