3a97b7d721d08ba386372d32c4a5f943a3087be6
Mazen Sayed <mazen.sayed@robustastudio.com>
2021-12-29 21:35:01 +0200

    added permit user

--- backend/api/user/controller.js
+++ backend/api/user/controller.js
@@ -13,13 +13,31 @@ module.exports.get_all_users = async (req, res) => {
 
 module.exports.get_user_by_id = async (req, res) => {
   try {
+    console.log("iiiiiiiiiiidddddddddd ", req.params.id);
     let user = await User.findOne({
       where: { id: req.params.id },
     });
-
-    res.status(200).json(user);
+    console.log("baaaaaaaaaaaaaaaaaaaaaaaaaaaack ", user);
+    if (user) res.status(200).send(user);
+    else {
+      res.status(404);
+    }
   } catch (e) {
     console.log(e);
     res.status(404).json({ message: e.message });
   }
 };
+
+module.exports.permit_user = async (req, res) => {
+  try {
+    let owner = await User.findOne({where: {id: req.body.user_id}});
+    for(let user_id of req.body.user_ids){
+      let user = await User.findOne({where: {id: user_id}});
+      await user.addOwner(owner);
+    }
+    res.json({"message": "permessions added"});
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({"error": e.message});
+  }
+}
